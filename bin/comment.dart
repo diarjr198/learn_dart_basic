d6a40df4668d6c298519c3a44337afce9ece63af
Diar J R <diar.student@gmail.com>
2024-01-02 23:10:28 +0700

    Add: membuat komentar

---
+++ bin/comment.dart
@@ -0,0 +1,12 @@
+/// this is main function
+/// this is executed by dart
+void main() {
+  // this is name variable
+  var name = "Diar Julianto Rahadi";
+
+  /** 
+   * this is multiline comment
+   * you can add more here
+   */
+  print(name);
+}
